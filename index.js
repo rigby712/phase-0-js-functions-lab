69957944efdf3a349a9acd666cd576638a32bb97
rigby712 <Samandrew304@gmail.com>
2025-03-23 22:07:37 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,24 @@
+function calculateTax (amount){
+    return amount * .1
+}
+function convertToUpperCase (text){
+    return  text.toUpperCase();
 
-
+}
+function findMaximum (num1,num2){
+    if (num1>num2){
+        return num1
+    }
+    return num2
+}
+function isPalindrome (word){
+    const cleanedWord = word.replace(/[^A-Za-z0-9]/g, '').toLowerCase();
+  const reversedWord = cleanedWord.split('').reverse().join('');
+  return cleanedWord === reversedWord;
+}
+function calculateDiscountedPrice(originalPrice, discountPercentage) {
+    return originalPrice - (originalPrice * discountPercentage / 100);
+  }
 
 
 
